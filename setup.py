5f32e9531ab99ff66a71f1a65e4a9bda110ade3a
Liam DeVoe <orionldevoe@gmail.com>
2020-05-25 16:46:16 -0400

    bump circleguard requirement to 4.2.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     download_url = "https://github.com/circleguard/circlevis/tarball/v" + VERSION,
     packages=find_packages(),
     install_requires=[
-        "circleguard ~= 4.0.2",
+        "circleguard ~= 4.2.0",
         "slider ~= 0.2.1"
     ]
 )
