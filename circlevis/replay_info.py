831373c10a9c9a64902c4f6ddfe9fb9052e38ac5
Liam DeVoe <orionldevoe@gmail.com>
2023-08-28 19:51:25 -0400

    reorganize imports

--- circlevis/replay_info.py
+++ circlevis/replay_info.py
@@ -1,9 +1,10 @@
 from functools import partial
 
+from PyQt6.QtWidgets import (QLabel, QVBoxLayout, QFrame, QAbstractItemView,
+    QTableWidget, QTableWidgetItem, QGridLayout)
 from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6.QtGui import QCursor
-from PyQt6.QtWidgets import (QLabel, QVBoxLayout, QFrame, QAbstractItemView,
-                             QTableWidget, QTableWidgetItem, QGridLayout)
+
 from circleguard import KeylessCircleguard, JudgmentType, convert_statistic
 
 from circlevis.widgets import CheckboxSetting, PushButton
